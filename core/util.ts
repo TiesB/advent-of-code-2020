9d1d0df4a6b92eb2bfdfcb59504055da9b4c0bce
Ties Bolding <tiesnld+git@gmail.com>
2020-12-14 08:04:25 +0100

    Automatically get input data

--- core/util.ts
+++ core/util.ts
@@ -1,13 +1,23 @@
 import * as fs from "fs";
+import axios from "axios";
 
 export function getAllDays(): number[] {
   const dir = fs.readdirSync(".");
   return dir.filter((d) => Number.isInteger(Number(d))).map((d) => Number(d));
 }
 
-export function generateDay(day: number): void {
+export async function generateDay(day: number): Promise<void> {
   fs.mkdirSync(`./${day}`);
-  fs.writeFileSync(`./${day}/input.txt`, "");
+  fs.writeFileSync(
+    `./${day}/input.txt`,
+    (
+      await axios.get(`https://adventofcode.com/2020/day/${day}/input`, {
+        headers: {
+          Cookie: `session=${process.env.SESSION_COOKIE}`,
+        },
+      })
+    ).data
+  );
   const template = fs.readFileSync("./core/_template.ts.txt", "utf-8");
   const output = template.replace("__day__", String(day));
   fs.writeFileSync(`./${day}/index.ts`, output);
--- index.ts
+++ index.ts
@@ -1,40 +1,46 @@
 import * as fs from "fs";
+import { config } from "dotenv";
 import { generateDay, getAllDays, printUsage } from "./core/util";
 
-let days = getAllDays();
-const inputDays = process.argv.slice(2).map((i) => {
-  const r = Number(i);
-  if (!Number.isInteger(r)) {
-    console.error(`Argument ${i} is not a valid integer number.`);
-    printUsage();
-    process.exit(1);
-  }
-  return Number(i);
-});
-if (inputDays.length) {
-  for (const day of inputDays) {
-    if (!days.includes(day)) {
-      generateDay(day);
+config();
+
+async function bootstrap() {
+  let days = getAllDays();
+  const inputDays = process.argv.slice(2).map((i) => {
+    const r = Number(i);
+    if (!Number.isInteger(r)) {
+      console.error(`Argument ${i} is not a valid integer number.`);
+      printUsage();
+      process.exit(1);
     }
+    return Number(i);
+  });
+  if (inputDays.length) {
+    for (const day of inputDays) {
+      if (!days.includes(day)) {
+        await generateDay(day);
+      }
+    }
+
+    days = inputDays;
   }
 
-  days = inputDays;
-}
+  for (const day of days) {
+    const { default: dayClass } = require(`./${day}`);
+    const dayInstance = new dayClass();
 
-for (const day of days) {
-  const { default: dayClass } = require(`./${day}`);
-  const dayInstance = new dayClass();
-
-  const lines = fs.readFileSync(`./${day}/input.txt`, "utf8").split("\n");
-  console.log(
-    `Day ${day}:\n\t1: ${dayInstance.solve1(
-      dayInstance.filterInput
-        ? dayInstance.filterInput(lines.map(dayInstance.parseLine))
-        : lines.map(dayInstance.parseLine)
-    )}\n\t2: ${dayInstance.solve2(
-      dayInstance.filterInput
-        ? dayInstance.filterInput(lines.map(dayInstance.parseLine))
-        : lines.map(dayInstance.parseLine)
-    )}`
-  );
+    const lines = fs.readFileSync(`./${day}/input.txt`, "utf8").split("\n");
+    console.log(
+      `Day ${day}:\n\t1: ${dayInstance.solve1(
+        dayInstance.filterInput
+          ? dayInstance.filterInput(lines.map(dayInstance.parseLine))
+          : lines.map(dayInstance.parseLine)
+      )}\n\t2: ${dayInstance.solve2(
+        dayInstance.filterInput
+          ? dayInstance.filterInput(lines.map(dayInstance.parseLine))
+          : lines.map(dayInstance.parseLine)
+      )}`
+    );
+  }
 }
+bootstrap();
